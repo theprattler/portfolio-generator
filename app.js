3aa26284110b85563d5e048505f5c592239c6d1a
The Prattler <jmichaelsmith@juno.com>
2021-11-29 12:50:32 -0500

    generated html page

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    console.log('===============');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
